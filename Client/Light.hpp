07b53cdcff4819869d7dada4ea0de138601c77a6
Andy Hwang <a-jih@users.noreply.github.com>
2019-04-17 18:04:04 -0700

    ajh - added base Light class

---
+++ Client/Light.hpp
@@ -0,0 +1,56 @@
+/**
+ * Light.hpp
+ */
+
+#pragma once
+
+#include "Shader.hpp"
+#include <memory>
+#include <string>
+
+struct LightIntensity
+{
+  glm::vec3 ambient;
+  glm::vec3 diffuse;
+  glm::vec3 specular;
+};
+
+class Light
+{
+public:
+  Light() = default;
+  ~Light() = default;
+
+  virtual void setUniforms(std::unique_ptr<Shader> const &shader);
+  virtual void update();
+
+  /**
+   * \brief Draws a representation of the light within the scene. For debugging purposes.
+   * \param shader(std::unique_ptr<Shader>const&) The shader program to render the light.
+   */
+  virtual void draw(std::unique_ptr<Shader> const &shader);
+
+  /**
+  * \brief Sets the uniform names for the light.
+  * \param name(std::string) The uniform name of the light.
+  */
+  virtual void set_name(const std::string &name);
+  void set_intensity(const LightIntensity &intensity);
+  void set_ambient(float r, float g, float b);
+  void set_diffuse(float r, float g, float b);
+  void set_specular(float r, float g, float b);
+
+protected:
+
+  // Uniform names
+  std::string _name;
+  std::string _name_ambient;
+  std::string _name_diffuse;
+  std::string _name_specular;
+
+  // Light properties
+  LightIntensity _intensity;
+  
+  // Rendering information (for debugging purposes)
+  glm::mat4 _world_mtx;
+};
