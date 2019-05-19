629dc3a0632ba8e52e43af686a6c28f32a122a5f
Andy Hwang <a-jih@users.noreply.github.com>
2019-05-19 14:58:00 -0700

    ajh - implementing particle physics

--- Client/ParticleSystem.cpp
+++ Client/ParticleSystem.cpp
@@ -1,7 +1,91 @@
+/**
+ * ParticleSystem.cpp
+ */
+
 #include "ParticleSystem.hpp"
+
+#include <algorithm>
 
 ParticleSystem::ParticleSystem()
 {
+  _particles.resize(kMaxParticles);
+  _position_data.resize(kMaxParticles);
+}
+
+void ParticleSystem::Reset()
+{
+  glGenVertexArrays(1, &_vao);
+  glBindVertexArray(_vao);
+
+  // Generate buffer of particle positions
+  glGenBuffers(1, &_position_buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, _position_buffer);
+  glBufferData(GL_ARRAY_BUFFER, kMaxParticles * 3 * sizeof(GLfloat), nullptr, GL_STREAM_DRAW);
+
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
+  glVertexAttribDivisor(0, 1);
+
+  glBindVertexArray(0);
+}
+
+void ParticleSystem::Update(float delta_time, const glm::vec3 &camera_position)
+{
+  unsigned int remaining_particles = _live_particles;
+  unsigned int particle_index = 0;
+
+  // Update physics
+  for (int i = 0; i < _live_particles; i++)
+  {
+    Particle p = _particles[i];
+
+    p.life -= delta_time;
+    if (p.life > 0.0f)
+    {
+      // Determine acceleration
+      glm::vec3 accel{ 0.0f };
+      accel = physics::gravity;
+
+      // Integrate velocity and position
+      p.velocity += accel * delta_time;
+      p.position += p.velocity * delta_time;
+
+      // Update particle distance from camera for sorting
+      p.camera_distance = glm::length(p.position - camera_position);
+
+      // Update particle position buffer data
+      _position_data[particle_index] = p.position;
+
+      particle_index++;
+    }
+    else
+    {
+      p.is_live = false;
+      p.camera_distance = -1.0f;
+
+      remaining_particles--;
+    }
+
+    // Update particle p
+    _particles[i] = p;
+
+    // Update number of live particles
+    _live_particles = remaining_particles;
+  }
+
+  // Update buffer data
+  glBindBuffer(GL_ARRAY_BUFFER, _position_buffer);
+  glBufferData(GL_ARRAY_BUFFER, kMaxParticles * 3 * sizeof(GLfloat), nullptr, GL_STREAM_DRAW);
+  glBufferSubData(GL_ARRAY_BUFFER, 0, _live_particles * sizeof(GLfloat) * 3, _position_data.data());
+}
+
+void ParticleSystem::Draw()
+{
+  glBindVertexArray(_vao);
+
+  glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 1, _live_particles);
+
+  glBindVertexArray(0);
 }
 
 glm::vec2 ParticleSystem::particle_size() const
@@ -25,6 +109,13 @@ void ParticleSystem::set_particle_size(float x, float y)
   _particle_size.y = y;
 }
 
+void ParticleSystem::set_particle_texture(const char * path)
+{
+  _particle_texture.id = LoadTextureFromFile(path, "./Resources/Textures");
+  _particle_texture.type = TextureType::DIFFUSE;
+  _particle_texture.path = std::string(path);
+}
+
 void ParticleSystem::set_lifespan(float lifespan)
 {
   _lifespan = lifespan;
@@ -33,4 +124,9 @@ void ParticleSystem::set_lifespan(float lifespan)
 void ParticleSystem::set_rate(unsigned int rate)
 {
   _rate = rate;
+}
+
+void ParticleSystem::SortParticles()
+{
+  std::sort(_particles.begin, _particles.end);
 }
--- Client/ParticleSystem.hpp
+++ Client/ParticleSystem.hpp
@@ -5,25 +5,61 @@
 #pragma once
 
 #include "Texture.hpp"
+#include "Physics.hpp"
+#include <vector>
+
+struct Particle
+{
+  glm::vec3 position;
+  glm::vec3 velocity;
+  float life;
+  float camera_distance = -1.0f;
+  bool is_live = false;
+
+  bool operator<(Particle &p)
+  {
+    this->camera_distance > p.camera_distance;
+  }
+};
 
 class ParticleSystem
 {
 public:
   ParticleSystem();
 
+  void Reset();
+  void Update(float delta_time, const glm::vec3 &camera_position);
+  void Draw();
+
   glm::vec2 particle_size() const;
   float lifespan() const;
   unsigned int rate() const;
 
   void set_particle_size(float x, float y);
+  void set_particle_texture(const char *path);
   void set_lifespan(float lifespan);
   void set_rate(unsigned int rate);
 
 private:
-  glm::vec2 _particle_size;  ///< Dimensions of a particle.
-  Texture _particle_texture; ///< Particle texture.
-  float _lifespan;           ///< Lifespan of a particle in seconds
-  unsigned int _rate;        ///< Rate of emission in particles per second.
+  const unsigned int kMaxParticles = 1000;
 
+  // System properties
+  glm::vec2 _particle_size;     ///< Dimensions of a particle.
+  Texture _particle_texture;    ///< Particle texture.
+  float _lifespan;              ///< Lifespan of a particle in seconds
+  unsigned int _rate;           ///< Rate of emission in particles per second.
+  unsigned int _live_particles; ///< Total number of active particles
 
+  // Physics
+  glm::vec3 _direction; ///< Direction of particle emission.
+  float _particle_mass; ///< Particle mass in kg.
+
+  // Rendering
+  GLuint _vao;
+  GLuint _position_buffer;
+
+  std::vector<Particle> _particles;
+  std::vector<glm::vec3> _position_data;
+
+  void SortParticles();
 };
