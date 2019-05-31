090f28c9e1196b104975313a231efb5b28733dde
johnmurcko <johnjosephmurcko@gmail.com>
2019-05-31 15:18:05 -0700

    created textures for skills

--- Client/GUIManager.cpp
+++ Client/GUIManager.cpp
@@ -486,8 +486,6 @@ void GuiManager::initLobbyScreen() {
 	_switchSidesButton->setBackgroundTexture(switchUnfocused, switchFocused, switchPushed);
 	_switchSidesButton->alpha = 1;
 	_switchSidesButton->setTheme(new nanogui::Theme(_screen->nvgContext()));
-	_switchSidesButton->theme()->mTextColor = nanogui::Color(0, 0, 255, 255);
-	_switchSidesButton->theme()->mTextColorShadow = nanogui::Color(0, 0, 0, 0);
 	_switchSidesButton->setFixedHeight(40);
 	_switchSidesButton->setFixedWidth(100);
 
@@ -514,8 +512,6 @@ void GuiManager::initLobbyScreen() {
 	_readyButton->setBackgroundTexture(readyUnfocused, readyFocused, readyPushed);
 	_readyButton->alpha = 1;
 	_readyButton->setTheme(new nanogui::Theme(_screen->nvgContext()));
-	_readyButton->theme()->mTextColor = nanogui::Color(0, 0, 255, 255);
-	_readyButton->theme()->mTextColorShadow = nanogui::Color(0, 0, 0, 0);
 	_readyButton->setFixedHeight(40);
 	_readyButton->setFixedWidth(100);
 
